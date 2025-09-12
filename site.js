d7bd69c978966167ad7597042ba3b5c879c091cd
Justice Ross <justice.ross2647@fvtc.edu>
2025-09-11 23:13:33 -0500

    Added welcome message

--- site.js
+++ site.js
@@ -1,1 +1,13 @@
 console.log('Hello Ryan :D')
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('#welcome p')
+
+if (isMorning) welcome.textContent = 'Welcome, Good Morning!'
+else if (isAfternoon) welcome.textContent = 'Welcome, Good Afternoon!'
+else if (isEvening) welcome.textContent = 'Welcome, Good Evening!'
